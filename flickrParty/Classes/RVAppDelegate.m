94b3d9d598eb281624d9634e6485bf8ab240b4eb
Rémy Virin <remy@remyvirin.com>
2013-06-25 19:19:11 +0200

    add a storyboard, will be easier for collection view

--- flickrParty/Classes/RVAppDelegate.m
+++ flickrParty/Classes/RVAppDelegate.m
@@ -14,12 +14,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-
-    
-    self.viewController = [[RVViewController alloc] initWithNibName:@"RVViewController" bundle:nil];
-    self.window.rootViewController = self.viewController;
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
